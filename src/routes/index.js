c8dab5cfd21368b31c241ce0e7ea84f79905091b
Shivesh Tiwari <algomonk016@gmail.com>
2021-11-15 09:18:42 +0530

    ✨ added routes

---
+++ src/routes/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route
+} from "react-router-dom";
+import Guest from '../container/pages/guest';
+import Feedback from '../container/pages/guest/Feedback';
+import Restaurant from '../container/pages/guest/Restaurant';
+import Login from '../container/pages/login';
+import GuestLogin from '../container/pages/login/GuestLogin';
+import ReceptionLogin from '../container/pages/login/ReceptionLogin';
+import Reception from '../container/pages/reception';
+
+const PageRoutes = () => {
+    return (
+        <Router>
+            <Routes>
+                {/* login */}
+                <Route path="/login" element={<Login />} />
+                <Route path="/login/guest" element={<GuestLogin />} />
+                <Route path="/login/reception" element={<ReceptionLogin />} />
+
+                {/* landing pages */}
+                <Route path="/guest" element={<Guest />} />
+                <Route path="/reception" element={<Reception />} />
+
+                {/* for guest */}
+                <Route path="/guest/feedback" element={<Feedback />} />
+                <Route path="/guest/restaurant" element={<Restaurant />} />
+
+            </Routes>
+        </Router>
+    )
+}
+
+export default PageRoutes
