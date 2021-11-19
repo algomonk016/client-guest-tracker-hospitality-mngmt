a60163ff206519ee666d1bb244a174490039a18a
Shivesh Tiwari <algomonk016@gmail.com>
2021-11-19 08:16:00 +0530

    ✨ added constants

---
+++ src/utils/constants.js
@@ -0,0 +1,13 @@
+export const constants = {
+    name: 'Name',
+    address: 'Address',
+    contactNo: 'Contact Number',
+    inTime: 'Time In',
+    outTime: 'Time Out',
+    gender: 'Gender',
+    room: 'Room',
+    sn: 'S.No',
+    age: 'Age',
+    iskeSaath: 'With him/her',
+    action: 'Actions'
+}
