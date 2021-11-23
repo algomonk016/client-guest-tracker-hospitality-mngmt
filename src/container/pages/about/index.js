48306ef82c11a5615910270f3160d388baa309f4
Shivesh Tiwari <algomonk016@gmail.com>
2021-11-23 11:49:07 +0530

    ✨ added about page

--- src/container/pages/about/index.js
+++ src/container/pages/about/index.js
@@ -1,22 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { constants } from '../../../utils/constants'
+import Modules from './components/Modules'
+import Objective from './components/Objective'
 
 const About = () => {
     return (
-        <div className="p-10">
-            about page contains
+        <div className="min-h-screen bg-white">
 
-            <ul>
-
-                <li> er diagrams </li>
-                <li> features of project </li>
-                <li> future plans </li>
-                <li> etc... </li>
-            </ul>
-
-            <div>
-                <Link to='/login'>Go to login page</Link>
+            {/* navbar */}
+            <div className="rounded-b-lg bg-purple-200 py-5 flex justify-between my-0 shadow">
+                <div className="uppercase text-2xl font-serif text-purple-600 ml-5 mt-1">{constants.projectName}</div>
+                <Link className="px-5 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-full focus:shadow-outline hover:bg-indigo-800 mr-8 py-2 cursor-pointer" to='/login'>Contiue to project</Link>
             </div>
+
+            <Objective />
+            <Modules />
+
         </div>
     )
 }
